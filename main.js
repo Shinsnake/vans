e4b63746a9d72cba9847af3a2f88cd4b4c158ce1
Shinsnake <sizleyshin@gmail.com>
2023-10-06 10:06:20 +0200

    add countdown+ sticky final render

--- main.js
+++ main.js
@@ -13,3 +13,20 @@ let x = setInterval(function() {
  
   document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 }, 1000);
+
+let mybutton = document.getElementById("btn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
